0b81328caae26ddd399ad4c608b8aac39d6d0234
HernanJMF <97522680+HernanJMF@users.noreply.github.com>
2025-03-25 23:51:34 -0500

    UPDATE: Remove horizontal mode lock code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './core/services/users/user.service';
-import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
-import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -13,34 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private platform: Platform,
-    private screenOrientation: ScreenOrientation
   ) {
     this.isLogged = this.userService.isAuthenticated;
 
-    this.platform.ready().then(() => {
-      requestAnimationFrame(() => {
-        try {
-          this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-        } catch (e) {
-          console.warn('No se pudo bloquear orientación:', e);
-        }
-
-        // 🔄 Escucha cambios de orientación y re-bloquea si es necesario
-        this.screenOrientation.onChange().subscribe(() => {
-          const current = this.screenOrientation.type;
-          console.log('Orientación actual:', current);
-
-          if (current.includes('landscape')) {
-            try {
-              this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-            } catch (e) {
-              console.warn('No se pudo re-bloquear orientación:', e);
-            }
-          }
-        });
-      });
-    });
   }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,12 @@ import { LoadingComponent } from './shared/components/loading/loading.component'
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { AuthInterceptor } from './core/http-interceptors/auth/auth.interceptor';
 import { BackgroundMode } from '@awesome-cordova-plugins/background-mode/ngx';
-import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @NgModule({
   declarations: [AppComponent, LoadingComponent, MessagesComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, HttpClientModule, ProgressSpinnerModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    ScreenOrientation,
     BackgroundMode,
     {
       provide: HTTP_INTERCEPTORS,
